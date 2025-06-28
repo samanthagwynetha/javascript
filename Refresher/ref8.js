8f88d195fecc316e459f4318b12b8ddd4c148c4b
samanthagwynetha <samanthagwynetha@gmail.com>
2025-06-28 20:00:23 +0800

    Refresher8: Arrays

--- Refresher/ref8.js
+++ Refresher/ref8.js
@@ -1,4 +1,19 @@
 //Refresher8: Arrays
 
 const fruits = ["Apple", "Banana", "Mango"];
-console.log(fruits);
+
+// for (let i = 1; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+fruits.push("Durian", "Avocado", "Dragonfruit", "Kiwi");
+fruits.pop();
+fruits.shift();
+fruits.unshift("Pineapple", "Grapes");
+fruits.splice(2, 1, "Orange", "Peach");
+
+
+
+for (let fruit of fruits) {
+    console.log(fruit);
+}
