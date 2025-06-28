e7b0d6af27ff4c8904842ce3ae841c111b6356c1
samanthagwynetha <samanthagwynetha@gmail.com>
2025-06-28 22:55:59 +0800

    Refresher 10: Set

---
+++ Refresher/ref10.js
@@ -0,0 +1,35 @@
+//Refresher10: Set
+
+// const fruits = new Set();
+//     fruits.add("Apple");
+//     fruits.add("Banana");
+//     fruits.add("Apple");
+//     fruits.add("Mango");
+//     fruits.add("Durian");
+
+//     fruits.delete("Banana");
+//     // console.log(fruits.has("Apple"));
+//     // console.log(fruits); 
+
+//     for (let fruit of fruits) {
+//         console.log(fruit);
+//     }
+
+
+// /*
+// Challenge: Remove Duplicates from an Array Using Set
+// */
+// const numbers = [1, 2, 3, 4, 5, 1, 1, 2, 3, 6];
+// const removeDuplicate = new Set(numbers);
+// console.log(removeDuplicate);
+
+
+// const items = ["pen", "book", "pen", "notebook", "book", "pencil"];
+// const schoolSupplies = new Set(items);
+// console.log(schoolSupplies)
+
+//  Task: Use a Set to count how many unique letters are in the word.
+// const word = "samantha arsua";
+// const uniqueLetters = new Set(word);
+// console.log(`Unique letters are ${word}: ${uniqueLetters.size}`)
+// console.log(uniqueLetters)
